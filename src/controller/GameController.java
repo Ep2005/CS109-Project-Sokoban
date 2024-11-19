755c1d3cd8edb9968a277236a54e9e912d87a6ae
Ernest Phang <Ernest.p2005@outlook.com>
2024-11-19 13:48:04 +0800

    Added box moving mechanism,  win/lose conditions and register system

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Direction;
 import model.MapMatrix;
+import view.game.Box;
 import view.game.GamePanel;
 import view.game.GridComponent;
 import view.game.Hero;
@@ -33,9 +34,12 @@ public class GameController {
         //target row can column.
         int tRow = row + direction.getRow();
         int tCol = col + direction.getCol();
+        int uRow = tRow + direction.getRow();
+        int uCol = tCol + direction.getCol();
         GridComponent targetGrid = view.getGridComponent(tRow, tCol);
         int[][] map = model.getMatrix();
         if (map[tRow][tCol] == 0 || map[tRow][tCol] == 2) {
+
             //update hero in MapMatrix
             model.getMatrix()[row][col] -= 20;
             model.getMatrix()[tRow][tCol] += 20;
@@ -45,11 +49,84 @@ public class GameController {
             //Update the row and column attribute in hero
             h.setRow(tRow);
             h.setCol(tCol);
+            if (finish()) {
+                System.out.println("you win, next level");
+            }
+            else if (gameover()) {
+                System.out.println("you lose");
+            }
+
+            return true;
+        } else if (map[tRow][tCol] == 10 && (map[uRow][uCol] == 2 || map[uRow][uCol] == 0)) {
+            map[tRow][tCol] -= 10;
+            map[uRow][uCol] += 10;
+            Box x = view.getGridComponent(tRow, tCol).removeBoxFromGrid();
+            view.getGridComponent(uRow, uCol).setBoxInGrid(x);
+            model.getMatrix()[row][col] -= 20;
+            model.getMatrix()[tRow][tCol] += 20;
+            //Update hero in GamePanel
+            Hero h = currentGrid.removeHeroFromGrid();
+            targetGrid.setHeroInGrid(h);
+            //Update the row and column attribute in hero
+            h.setRow(tRow);
+            h.setCol(tCol);
+            if (finish()) {
+                System.out.println("you win, next level");
+            }
+            else if (gameover()) {
+                System.out.println("you lose");
+            }
+            return true;
+        } else if (map[tRow][tCol] == 12 && (map[uRow][uCol] == 2 || map[uRow][uCol] == 0)) {
+            map[tRow][tCol] -= 10;
+            map[uRow][uCol] += 10;
+            Box x = view.getGridComponent(tRow, tCol).removeBoxFromGrid();
+            view.getGridComponent(uRow, uCol).setBoxInGrid(x);
+            model.getMatrix()[row][col] -= 20;
+            model.getMatrix()[tRow][tCol] += 20;
+            Hero h = currentGrid.removeHeroFromGrid();
+            targetGrid.setHeroInGrid(h);
+            h.setRow(tRow);
+            h.setCol(tCol);
+            if (finish()) {
+                System.out.println("you win, next level");
+            }
+            else if (gameover()) {
+                System.out.println("you lose");
+            }
             return true;
         }
         return false;
+
     }
+
+    public boolean finish() {
+        int[][] matrix = model.getMatrix();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 10) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean gameover() {
+        int[][] matrix = model.getMatrix();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 10 && (matrix[i-1][j]==1 && matrix[i][j-1]==1 ||matrix[i-1][j]==1 && matrix[i][j+1]==1||matrix[i+1][j]==1 && matrix[i][j-1]==1 ||matrix[i+1][j]==1 && matrix[i][j+1]==1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
 
     //todo: add other methods such as loadGame, saveGame...
 
-}
--- src/view/login/LoginFrame.java
+++ src/view/login/LoginFrame.java
@@ -1,17 +1,21 @@
 package view.login;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 import view.FrameUtil;
 import view.level.LevelFrame;
-
+import java.io.IOException;
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
 
 
 public class LoginFrame extends JFrame {
     private JTextField username;
     private JTextField password;
     private JButton submitBtn;
-    private JButton resetBtn;
+    private JButton registerBtn;
     private LevelFrame levelFrame;
 
 
@@ -25,21 +29,79 @@ public class LoginFrame extends JFrame {
         password = FrameUtil.createJTextField(this, new Point(120, 80), 120, 40);
 
         submitBtn = FrameUtil.createButton(this, "Confirm", new Point(40, 140), 100, 40);
-        resetBtn = FrameUtil.createButton(this, "Reset", new Point(160, 140), 100, 40);
+        registerBtn = FrameUtil.createButton(this, "register", new Point(160, 140), 100, 40);
 
         submitBtn.addActionListener(e -> {
-            System.out.println("Username = " + username.getText());
-            System.out.println("Password = " + password.getText());
-            if (this.levelFrame != null) {
+            File file=new File("C:\\cs\\Demo\\acc.txt");
+            ArrayList<String> users=new ArrayList<>();
+            ArrayList<String> password=new ArrayList<>();
+            Scanner f;
+            try{
+                f = new Scanner(file);
+                while (f.hasNext()){
+                    users.add(f.next());
+                    f.next();}
+
+            }
+            catch (IOException g){
+                System.out.println("error");
+            }
+
+            try{
+                BufferedWriter a = new BufferedWriter(new FileWriter("acc.txt",true));
+                String username1 = username.getText();
+                if(!users.contains(username1)) {
+
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Username is already in use");
+                }
+                a.flush();
+                a.close();
+            }
+            catch (IOException ee){
+                System.out.println("error");
+            }
+
+            if (this.levelFrame != null) { //if cannot login this no work)
                 this.levelFrame.setVisible(true);
                 this.setVisible(false);
             }
             //todo: check login info
 
         });
-        resetBtn.addActionListener(e -> {
-            username.setText("");
-            password.setText("");
+        registerBtn.addActionListener(e -> {
+            File file=new File("C:\\cs\\Demo\\acc.txt");
+            ArrayList<String> users = new ArrayList<>();
+            Scanner f;
+            try{
+                f = new Scanner(file);
+                while (f.hasNext()){
+                    users.add(f.next());
+                    f.next();}
+
+            }
+            catch (IOException g){
+                System.out.println("error");
+            }
+
+            try{
+                BufferedWriter a = new BufferedWriter(new FileWriter("acc.txt",true));
+                String username1 = username.getText();
+                if(!users.contains(username1)) {
+                    String password1 = password.getText();
+                    a.write(username1 + " " + password1 + "\n");
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Username is already in use");
+                }
+                a.flush();
+                a.close();
+            }
+            catch (IOException ee){
+                System.out.println("error");
+            }
+
         });
 
         this.setLocationRelativeTo(null);
